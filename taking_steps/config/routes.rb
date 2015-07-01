48f57a3fc8486b4ba3862a18757ac526918346f2
Fureigh <info@fureigh.com>
2015-06-30 23:14:33 -0700

    Add minimum viable query path

--- taking_steps/config/routes.rb
+++ taking_steps/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root              'static_pages#home'
   get 'home' =>     'static_pages#home'
   get 'about' =>    'static_pages#about'
+  get 'query' =>    'static_pages#query'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
