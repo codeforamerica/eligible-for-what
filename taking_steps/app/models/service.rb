cb6890c5f67d5ee0e20f446f1ec8f15560d347da
Fureigh <info@fureigh.com>
2015-07-17 22:36:06 -0700

    [#30] Add initial validation for Service model

--- taking_steps/app/models/service.rb
+++ taking_steps/app/models/service.rb
@@ -1,2 +1,4 @@
 class Service < ActiveRecord::Base
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :description, presence: true, length: { maximum: 255 }
 end
--- taking_steps/test/models/service_test.rb
+++ taking_steps/test/models/service_test.rb
@@ -4,4 +4,32 @@ class ServiceTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @service = Service.new(name: 'Sample Service', description: 'This is an example of a service.', location: '12345 Main Street, Oakland, CA 94607', organization: 'Test Organization')
+  end
+
+  test "should be valid" do
+    assert @service.valid?
+  end
+
+  test "name should be present" do
+    @service.name = ''
+    assert_not @service.valid?
+  end
+
+  test "name should not be too long" do
+    @service.name = 'z' * 256
+    assert_not @service.valid?
+  end
+
+  test "description should be present" do
+    @service.description = ''
+    assert_not @service.valid?
+  end
+
+  test "description should not be too long" do
+    @service.description = 'a' * 256
+    assert_not @service.valid?
+  end
 end
