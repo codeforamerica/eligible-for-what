a96bc88e666cf79650b497fbd6d83c0bd1144203
Fureigh <info@fureigh.com>
2015-07-19 15:02:15 -0700

    Finish removing earlier query-based approach

--- taking_steps/config/routes.rb
+++ taking_steps/config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   root              'static_pages#home'
   get 'home' =>     'static_pages#home'
   get 'about' =>    'static_pages#about'
-  post 'query' =>    'services#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
