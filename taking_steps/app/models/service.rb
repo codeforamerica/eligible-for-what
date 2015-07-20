98777e4252f791bd061e72d504286b99403b5543
Fureigh <info@fureigh.com>
2015-07-19 22:18:06 -0700

    [#40] Link service address to Google Map. Closes #40

--- taking_steps/app/models/service.rb
+++ taking_steps/app/models/service.rb
@@ -1,4 +1,15 @@
 class Service < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 255 }
   validates :description, presence: true, length: { maximum: 255 }
+
+  # Converts an address to its corresponding Google Maps link.
+  def google_maps_link(location)
+    # Start: 155 9th St, San Francisco, CA
+    # End: https://www.google.com/maps/place/155+9th+St+San+Francisco+CA
+
+    # @someday: Validate location.
+    gmaps_url = 'https://www.google.com/maps/place/' + location.tr(' ', '+')
+
+  end
+
 end
